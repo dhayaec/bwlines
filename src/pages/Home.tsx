299f50107567420aff966edfaedc6f447504a6cf
Dhayanandhan <dhayaec@gmail.com>
2018-09-02 19:15:46 +0530

    add reakit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import { RouteComponentProps } from '@reach/router';
 import * as React from 'react';
+import { Arrow, Avatar, Block, Button, InlineFlex, styled } from 'reakit';
 import { getRandomJoke } from '../api/random-jokes';
 import RenderJoke from '../blocks/RenderJoke';
 import HeaderMenu from '../components/HeaderMenu';
 import { Title } from '../components/Title';
 
 interface HomeProps {}
+
+const ButtonRounded = styled(Button)`
+  border-radius: 1.25em;
+  padding: 0 1.375em;
+  background-color: orangered;
+  color: white;
+`;
 
 class Home extends React.Component<HomeProps & RouteComponentProps> {
   state = {
@@ -37,9 +45,37 @@ class Home extends React.Component<HomeProps & RouteComponentProps> {
       <div>
         <HeaderMenu />
         <Title>Hello World!</Title>
+        <Button as="a" href="https://github.com/reakit/reakit" target="_blank">
+          GitHub
+        </Button>
+        <ButtonRounded>Rounded</ButtonRounded>
         <h1>Hello</h1>
         <h2>Welcome to my website</h2>
+        <InlineFlex justifyContent="space-between" width="100%">
+          <Block
+            width="33%"
+            height="100px"
+            backgroundColor="rgb(219, 112, 147)"
+          />
+          <Block
+            width="33%"
+            height="100px"
+            backgroundColor="rgb(219, 112, 198)"
+          />
+          <Block
+            width="33%"
+            height="100px"
+            backgroundColor="rgb(205, 112, 219)"
+          />
+        </InlineFlex>
         <button onClick={this.loadJoke}>Refresh</button>
+        <Arrow />
+
+        <Avatar
+          src="https://placekitten.com/150/200"
+          alt="Kitten"
+          fontSize={40}
+        />
         <RenderJoke joke={joke} loading={loading} />
       </div>
     );
