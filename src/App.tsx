e7f05486056b6d9045114e92d3e05f3b315f0b98
Dhayanandhan <dhayaec@gmail.com>
2018-09-02 19:15:46 +0530

    add reakit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Router } from '@reach/router';
 import * as React from 'react';
-import { Provider } from 'rebass';
 import About from './pages/About';
 import Cart from './pages/Cart';
 import Contact from './pages/Contact';
@@ -13,18 +12,16 @@ import Products from './pages/Products';
 class App extends React.Component {
   public render() {
     return (
-      <Provider>
-        <Router>
-          <Home path="/" />
-          <About path="/about" />
-          <Products path="/products" />
-          <Product path="/products/:productId" />
-          <Contact path="/contact" />
-          <Cart path="/cart" />
-          <Jokes path="/jokes/:categoryName" />
-          <NotFound default={true} />
-        </Router>
-      </Provider>
+      <Router>
+        <Home path="/" />
+        <About path="/about" />
+        <Products path="/products" />
+        <Product path="/products/:productId" />
+        <Contact path="/contact" />
+        <Cart path="/cart" />
+        <Jokes path="/jokes/:categoryName" />
+        <NotFound default={true} />
+      </Router>
     );
   }
 }
